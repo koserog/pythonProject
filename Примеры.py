ac4e442ed35855a22461c02f98324e3ce8526dc5
Kirill <koserog20122@mail.ru>
2023-01-24 22:46:29 +0500

    Сделал херню

---
+++ Примеры.py
@@ -0,0 +1,64 @@
+# 001
+# firstname = input("Введите имя: ")
+# print("Привет", firstname)
+
+# 002
+# name1 = input("Введите имя: ")
+# name2 = input("Введитке фамилию: ")
+# print("Привет", name1, name2)
+
+#003
+#print("What do you call a bear with no teeth?\nA gummy bear!")
+
+#004
+#num1 = int(input("Введите первое число:  "))
+#num2 = int(input("Введите второе число:  "))
+#num3 = num1 + num2
+#print("Ровно", num3)
+
+
+# 005
+# num1 = int(input("Введите первое число:  "))
+# num2 = int(input("Введите второе число:  "))
+# num3 = int(input("Введите второе число:  "))
+# num4 = (num1 + num2) * num3
+# print("Ровно", num4)
+
+#006
+# word = "кусков"
+# new_num = (8, 0)
+# pizza = int(input("Сколько у тебя было кусков пиццы?  "))
+# eat = int(input("Сколько кусков пиццы ты съел?  "))
+# remains = pizza - eat
+# print("Кусков пиццы у тебя осталось: ", remains)
+
+#007
+# year = int(input("Введите ваш возраст: "))
+# name = input("Введите имя: ")
+# year = year + 1
+# print(name, "в следующем году тебе будет ", year)
+
+
+#008
+# score = float(input("Введите сумму счета:"))
+# company = int(input("Введите количество человек за столом:"))
+# division = score / company
+# print("Каждый из компании должен заплатить:", division )
+
+#009
+# day = int(input("Напишите количество дней: "))
+# hours = day * 24
+# minutes = day * 1440
+# seconds = day * 86400
+# print(hours, "часов, ",minutes,"минут и",seconds, "секунд, в данном количестве дней")
+
+#010
+#massa = float(input("Введите весс в килограмах "))
+#pound = massa * 2,204
+#print("В данном количестве килограмм, данное количетво фунтов:", pound)
+
+#011
+max = float(input("Введите число больше 100 "))
+min = float(input("Введите число меньше 10 "))
+conclusion = int(max // min)
+print("Данное число:", min,"помещается в числе", max,"данное количество раз", conclusion)
